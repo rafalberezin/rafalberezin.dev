c4e1c1f5bf15573d36f67408052981fefdc740d7
Rafał Berezin <rafalberezin@protonmail.com>
2025-01-24 11:04:25 +0100

    feat(article): add Article and BlockChildren helper components

---
+++ src/lib/components/article/Article.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import BlockChildren from './BlockChildren.svelte'
+	import type { Root } from 'mdast'
+
+	interface Props {
+		mdast: Root
+		accentColor: string
+	}
+
+	const { mdast, accentColor }: Props = $props()
+</script>
+
+<article style={`--accent-color: ${accentColor}`}>
+	<BlockChildren block={mdast} />
+</article>
+
+<style>
+	article {
+		width: 100%;
+		padding: 1rem;
+		padding-left: 3rem;
+		background-color: var(--mantle);
+	}
+</style>
---
+++ src/lib/components/article/BlockChildren.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { Parent } from 'mdast'
+
+	interface Props {
+		block: Parent
+	}
+
+	const { block }: Props = $props()
+	const { children } = block
+</script>
+
+{#each children as block}
+	{#if block.component}
+		<block.component {block} />
+	{/if}
+{/each}
